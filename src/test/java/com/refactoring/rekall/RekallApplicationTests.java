e3254bd7789a704d1937a92f14d275d916c14376
DESKTOP-24FQL1L\suu <su9155@naver.com>
2023-07-30 17:01:05 +0900

    no message

--- src/test/java/com/refactoring/rekall/RekallApplicationTests.java
+++ src/test/java/com/refactoring/rekall/RekallApplicationTests.java
@@ -2,8 +2,10 @@ package com.refactoring.rekall;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("bambi")
 class RekallApplicationTests {
 
 	@Test
